4e531b1683aab9f26509b6e9349cac7950061a06
Roy Marmelstein <marmelroy@gmail.com>
2016-11-14 08:31:40 +0100

    Add unit tests

--- ObjectiveKitTests/ObjectiveKitTests.swift
+++ ObjectiveKitTests/ObjectiveKitTests.swift
@@ -26,65 +26,38 @@ import MapKit
 
     let closureName = "random"
 
-
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
     dynamic func testSelector() {
         print("test selector")
     }
 
-    dynamic func swizzledSelector(){
-        print("swizzled selector")
-    }
-
     func testAddClosure() {
+        let methodExpectation = expectation(description: "Method was called")
         let objectiveView = ObjectiveClass<UIView>()
         objectiveView.addMethodToClass(closureName, implementation: {
-            print("hello world")
+            methodExpectation.fulfill()
         })
         let view = UIView()
         view.performMethod(closureName)
+        waitForExpectations(timeout: 0.1, handler:nil)
     }
 
     func testAddSelector() {
+        let view = UIView()
+        XCTAssertFalse(view.responds(to: #selector(testSelector)))
         let objectiveView = ObjectiveClass<UIView>()
         objectiveView.addSelectorToClass(#selector(testSelector), from: self)
-        let view = UIView()
-        view.perform(#selector(testSelector))
+        XCTAssert(view.responds(to: #selector(testSelector)))
     }
 
     func testIntrospection() {
         let objectiveView = ObjectiveClass<MKMapView>()
         let selectors = objectiveView.allSelectors()
-        print("==SELECTORS== \(selectors)")
+        XCTAssert(selectors.contains(NSSelectorFromString("layoutSubviews")))
         let protocols = objectiveView.allProtocols()
-        print("==PROTOCOLS== \(protocols)")
+        XCTAssert(protocols.contains("MKAnnotationManagerDelegate"))
         let properties = objectiveView.allProperties()
-        print("==PROPERTIES== \(properties)")
-    }
-
-    func testExchange() {
-        let objectiveView = ObjectiveClass<Subview>()
-        objectiveView.exchangeSelector(#selector(Subview.testSelector), with: #selector(Subview.swizzledSelector))
-        let subview = Subview()
-        subview.perform(#selector(Subview.testSelector))
-        
+        XCTAssert(properties.contains("mapRegion"))
     }
 
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
