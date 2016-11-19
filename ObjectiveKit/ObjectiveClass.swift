2a7c18f36ce4c2e4300632a25b5d88bad3e08ca1
CoderXpert <adnan.iiui@gmail.com>
2016-11-19 22:20:46 +0000

    Refactor to avoid nesting by using guard + by removing some unnecessary `{}`

--- ObjectiveKit/ObjectiveClass.swift
+++ ObjectiveKit/ObjectiveClass.swift
@@ -28,20 +28,17 @@ public class ObjectiveClass <T: NSObject>: ObjectiveKitRuntimeModification {
     ///
     /// - Returns: An array of instance variables.
     public var ivars: [String] {
-        get {
-            var count: CUnsignedInt = 0
-            var ivars = [String]()
-            let ivarList = class_copyIvarList(internalClass, &count)
-            for i in (0..<Int(count)) {
-                let unwrapped  = ivarList?[i].unsafelyUnwrapped
-                if let ivar = ivar_getName(unwrapped) {
-                    let string = String(cString: ivar)
-                    ivars.append(string)
-                }
-            }
-            free(ivarList)
-            return ivars
+        var count: CUnsignedInt = 0
+        var ivars = [String]()
+        let ivarList = class_copyIvarList(internalClass, &count)
+        for i in 0..<Int(count) {
+            let unwrapped  = ivarList?[i].unsafelyUnwrapped
+            guard let ivar = ivar_getName(unwrapped) else { continue }
+            let string = String(cString: ivar)
+            ivars.append(string)
         }
+        free(ivarList)
+        return ivars
     }
 
 
@@ -49,58 +46,49 @@ public class ObjectiveClass <T: NSObject>: ObjectiveKitRuntimeModification {
     ///
     /// - Returns: An array of selectors.
     public var selectors: [Selector]  {
-        get {
-            var count: CUnsignedInt = 0
-            var selectors = [Selector]()
-            let methodList = class_copyMethodList(internalClass, &count)
-            for i in (0..<Int(count)) {
-                let unwrapped  = methodList?[i].unsafelyUnwrapped
-                if let selector = method_getName(unwrapped) {
-                    selectors.append(selector)
-                }
-            }
-            free(methodList)
-            return selectors
+        var count: CUnsignedInt = 0
+        var selectors = [Selector]()
+        let methodList = class_copyMethodList(internalClass, &count)
+        for i in 0..<Int(count) {
+            let unwrapped  = methodList?[i].unsafelyUnwrapped
+            guard let selector = method_getName(unwrapped) else { continue }
+            selectors.append(selector)
         }
+        free(methodList)
+        return selectors
     }
 
     /// Get all protocols implemented by the class.
     ///
     /// - Returns: An array of protocol names.
     public var protocols: [String] {
-        get {
-            var count: CUnsignedInt = 0
-            var protocols = [String]()
-            let protocolList = class_copyProtocolList(internalClass, &count)
-            for i in (0..<Int(count)) {
-                let unwrapped  = protocolList?[i].unsafelyUnwrapped
-                if let protocolName = protocol_getName(unwrapped) {
-                    let string = String(cString: protocolName)
-                    protocols.append(string)
-                }
-            }
-            return protocols
+        var count: CUnsignedInt = 0
+        var protocols = [String]()
+        let protocolList = class_copyProtocolList(internalClass, &count)
+        for i in 0..<Int(count) {
+            let unwrapped  = protocolList?[i].unsafelyUnwrapped
+            guard let protocolName = protocol_getName(unwrapped) else { continue }
+            let string = String(cString: protocolName)
+            protocols.append(string)
         }
+        return protocols
     }
 
     /// Get all properties implemented by the class.
     ///
     /// - Returns: An array of property names.
     public var properties: [String] {
-        get {
-            var count: CUnsignedInt = 0
-            var properties = [String]()
-            let propertyList = class_copyPropertyList(internalClass, &count)
-            for i in (0..<Int(count)) {
-                let unwrapped  = propertyList?[i].unsafelyUnwrapped
-                if let propretyName = property_getName(unwrapped) {
-                    let string = String(cString: propretyName)
-                    properties.append(string)
-                }
-            }
-            free(propertyList)
-            return properties
+        var count: CUnsignedInt = 0
+        var properties = [String]()
+        let propertyList = class_copyPropertyList(internalClass, &count)
+        for i in 0..<Int(count) {
+            let unwrapped  = propertyList?[i].unsafelyUnwrapped
+            guard let propretyName = property_getName(unwrapped) else { continue }
+            let string = String(cString: propretyName)
+            properties.append(string)
         }
+        free(propertyList)
+        return properties
     }
 
 }
