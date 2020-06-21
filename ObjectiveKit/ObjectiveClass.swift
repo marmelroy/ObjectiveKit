a2cf120644c639c8bedec2f53d068444fc59b4d2
Koutarou ISHIKAWA <stein2nd@gmail.com>
2020-06-21 16:44:44 +0900

    - Swift version: from 3 to 5.
    - The location of definition `performMethod(_:)`: Moved from RuntimeModification.swift to NSObject.swift.
        NSObject.swift into added group 'Extensions', without Folder.
        RuntimeModification.swift into added group 'Protocols', without Folder.
    
    - Changed the implementation location of the following methods to ObjectiveClass and RuntimeClass: `addSelector(_:from:)`, `addMethod(_:implementation:)`, and `exchangeSelector(_:implementation:)`.
    
    - The parameter `implementation` of `addMethod(_:implementation:)`: correctted from `ImplementationBlock` to `@escaping @convention(block) () -> Void`.
    
    - The sources of variable `unwrapped` in `ivars`, `selectors`, `protocols`, and `properties` on ObjectiveClass: correctted from `ivarList?[i].unsafelyUnwrapped` to `propertyList![i].self`.
    
    - Since only calling MapKit in the Test class, added the MapKit.framework to the target "ObjectiveKit".

--- ObjectiveKit/ObjectiveClass.swift
+++ ObjectiveKit/ObjectiveClass.swift
@@ -1,39 +1,39 @@
-//
-//  UnregisteredClass.swift
-//  ObjectiveKit
-//
-//  Created by Roy Marmelstein on 11/11/2016.
-//  Copyright © 2016 Roy Marmelstein. All rights reserved.
-//
-
 import Foundation
 
-typealias ImplementationBlock = @convention(block) () -> Void
+//typealias ImplementationBlock = @convention(block) () -> Void
 
-/// An object that allows you to introspect and modify classes through the ObjC runtime.
+/**
+ An object that allows you to introspect and modify classes through the ObjC runtime.
+ - Migrator: ISHIKAWA Koutarou
+ - Migrated from Swift 3 to Swift 5: 2020/06/19
+ - Migrate Copyright: © 2020 ISHIKAWA Koutarou. All rights reserved.
+ - Original Author: Roy Marmelstein
+ - Original Created: 2016/11/11
+ - Original Copyright: © 2016 Roy Marmelstein All rights reserved. This software is licensed under the MIT License. Full details can be found in the README.
+ */
 public class ObjectiveClass <T: NSObject>: ObjectiveKitRuntimeModification {
 
     public var internalClass: AnyClass
 
-    // MARK: Lifecycle
+
+	//MARK: - Lifecycle
 
     /// Init
     public init() {
         self.internalClass = T.classForCoder()
     }
 
-    // MARK: Introspection
 
-    /// Get all instance variables implemented by the class.
-    ///
-    /// - Returns: An array of instance variables.
+	//MARK: - Introspection
+
+    /// All instance variables implemented by the class.
     public var ivars: [String] {
         get {
             var count: CUnsignedInt = 0
             var ivars = [String]()
-            let ivarList = class_copyIvarList(internalClass, &count)
+            let ivarList = class_copyIvarList(self.internalClass, &count)
             for i in (0..<Int(count)) {
-                let unwrapped  = ivarList?[i].unsafelyUnwrapped
+                let unwrapped: Ivar  = ivarList![i].self
                 if let ivar = ivar_getName(unwrapped) {
                     let string = String(cString: ivar)
                     ivars.append(string)
@@ -44,65 +44,75 @@ public class ObjectiveClass <T: NSObject>: ObjectiveKitRuntimeModification {
         }
     }
 
-
-    /// Get all selectors implemented by the class.
-    ///
-    /// - Returns: An array of selectors.
+    /// All selectors implemented by the class.
     public var selectors: [Selector]  {
         get {
             var count: CUnsignedInt = 0
             var selectors = [Selector]()
-            let methodList = class_copyMethodList(internalClass, &count)
+            let methodList = class_copyMethodList(self.internalClass, &count)
             for i in (0..<Int(count)) {
-                let unwrapped  = methodList?[i].unsafelyUnwrapped
-                if let selector = method_getName(unwrapped) {
-                    selectors.append(selector)
-                }
+                let unwrapped: Method  = methodList![i].self
+				selectors.append(method_getName(unwrapped))
             }
             free(methodList)
             return selectors
         }
     }
 
-    /// Get all protocols implemented by the class.
-    ///
-    /// - Returns: An array of protocol names.
+    /// All protocols implemented by the class.
     public var protocols: [String] {
         get {
             var count: CUnsignedInt = 0
             var protocols = [String]()
-            let protocolList = class_copyProtocolList(internalClass, &count)
+            let protocolList = class_copyProtocolList(self.internalClass, &count)
             for i in (0..<Int(count)) {
-                let unwrapped  = protocolList?[i].unsafelyUnwrapped
-                if let protocolName = protocol_getName(unwrapped) {
-                    let string = String(cString: protocolName)
-                    protocols.append(string)
-                }
+                let unwrapped: Protocol  = protocolList![i].self
+				let string: String = String.init(cString: protocol_getName(unwrapped))
+				protocols.append(string)
             }
             return protocols
         }
     }
 
-    /// Get all properties implemented by the class.
-    ///
-    /// - Returns: An array of property names.
+    /// All properties implemented by the class.
     public var properties: [String] {
         get {
             var count: CUnsignedInt = 0
             var properties = [String]()
-            let propertyList = class_copyPropertyList(internalClass, &count)
+            let propertyList = class_copyPropertyList(self.internalClass, &count)
             for i in (0..<Int(count)) {
-                let unwrapped  = propertyList?[i].unsafelyUnwrapped
-                if let propretyName = property_getName(unwrapped) {
-                    let string = String(cString: propretyName)
-                    properties.append(string)
-                }
+                let unwrapped: objc_property_t  = propertyList![i].self
+				let string: String = String.init(cString: property_getName(unwrapped))
+				properties.append(string)
             }
             free(propertyList)
             return properties
         }
     }
 
+
+	//MARK: - ObjectiveKitRuntimeModification (Runtime modification)
+
+	public func addSelector(_ selector: Selector, from originalClass: AnyClass) {
+		guard let method: Method = class_getInstanceMethod(originalClass, selector), let typeEncoding: UnsafePointer<Int8> = method_getTypeEncoding(method) else {
+			return
+		}
+		let implementation: IMP = method_getImplementation(method)
+		let string: String = String.init(cString: typeEncoding)
+		class_addMethod(self.internalClass, selector, implementation, string)
+	}
+
+	public func addMethod(_ identifier: String, implementation: @escaping @convention(block) () -> Void) {
+		let blockObject: AnyObject = unsafeBitCast(implementation, to: AnyObject.self)
+		let implementation: IMP = imp_implementationWithBlock(blockObject)
+		let selector: Selector = NSSelectorFromString(identifier)
+		let encoding: String = "v@:f"
+		class_addMethod(self.internalClass, selector, implementation, encoding)
+	}
+
+	public func exchangeSelector(_ aSelector: Selector, with otherSelector: Selector) {
+		let method: Method? = class_getInstanceMethod(self.internalClass, aSelector)
+		let otherMethod: Method? = class_getInstanceMethod(self.internalClass, otherSelector)
+		method_exchangeImplementations(method!, otherMethod!)
+	}
 }
-
-
--- ObjectiveKit/RuntimeModification.swift
+++ ObjectiveKit/RuntimeModification.swift
@@ -1,75 +1,35 @@
-//
-//  RuntimeModification.swift
-//  ObjectiveKit
-//
-//  Created by Roy Marmelstein on 14/11/2016.
-//  Copyright © 2016 Roy Marmelstein. All rights reserved.
-//
-
 import Foundation
 
+/**
+ - Migrator: ISHIKAWA Koutarou
+ - Migrated from Swift 3 to Swift 5: 2020/06/19
+ - Migrate Copyright: © 2020 ISHIKAWA Koutarou. All rights reserved.
+ - Original Created: 2016/11/14
+ - Original Copyright: © 2016 Roy Marmelstein All rights reserved. This software is licensed under the MIT License. Full details can be found in the README.
+ */
 public protocol ObjectiveKitRuntimeModification {
 
     var internalClass: AnyClass { get }
 
-    // MARK: Runtime modification
+
+	//MARK: - Runtime modification
 
     /// Add a selector that is implemented on another object to the current class.
-    ///
     /// - Parameters:
     ///   - selector: Selector.
     ///   - originalClass: Object implementing the selector.
     func addSelector(_ selector: Selector, from originalClass: AnyClass)
 
     /// Add a custom method to the current class.
-    ///
     /// - Parameters:
     ///   - identifier: Selector name.
     ///   - implementation: Implementation as a closure.
-    func addMethod(_ identifier: String, implementation: ImplementationBlock)
+    func addMethod(_ identifier: String, implementation: @escaping @convention(block) () -> Void)
 
     /// Exchange selectors implemented in the current class.
-    ///
     /// - Parameters:
     ///   - aSelector: Selector.
     ///   - otherSelector: Selector.
     func exchangeSelector(_ aSelector: Selector, with otherSelector: Selector)
 
 }
-
-extension ObjectiveKitRuntimeModification {
-
-    public func addSelector(_ selector: Selector, from originalClass: AnyClass) {
-        guard let method = class_getInstanceMethod(originalClass, selector), let implementation = method_getImplementation(method), let typeEncoding = method_getTypeEncoding(method) else {
-            return
-        }
-        let string = String(cString: typeEncoding)
-        class_addMethod(internalClass, selector, implementation, string)
-    }
-
-    public func addMethod(_ identifier: String, implementation: ImplementationBlock) {
-        let blockObject = unsafeBitCast(implementation, to: AnyObject.self)
-        let implementation = imp_implementationWithBlock(blockObject)
-        let selector = NSSelectorFromString(identifier)
-        let encoding = "v@:f"
-        class_addMethod(internalClass, selector, implementation, encoding)
-    }
-
-    public func exchangeSelector(_ aSelector: Selector, with otherSelector: Selector) {
-        let method = class_getInstanceMethod(internalClass, aSelector)
-        let otherMethod = class_getInstanceMethod(internalClass, otherSelector)
-        method_exchangeImplementations(method, otherMethod)
-    }
-
-}
-
-public extension NSObject {
-
-    /// A convenience method to perform selectors by identifier strings.
-    ///
-    /// - Parameter identifier: Selector name.
-    public func performMethod(_ identifier: String) {
-        perform(NSSelectorFromString(identifier))
-    }
-}
-
